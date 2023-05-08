f23a540ca45ce52c30a34a0fc106cff3d1fe852d
Md-arefin <ansariarefin617@gmail.com>
2023-05-09 02:55:34 +0600

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware 
 
@@ -44,7 +44,12 @@ async function run() {
         res.send(result);
     })
 
-
+    app.delete('/coffee/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id)};
+        const result = await coffeeCollection.deleteOne(query);
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
